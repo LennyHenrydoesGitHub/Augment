4cbf192ff9afd2ced71ad6d3b32e393a56bdd657
Neymat <wq20750@bristol.ac.uk>
2024-03-11 15:15:06 +0000

    code

--- Yolov8.py
+++ Yolov8.py
@@ -17,5 +17,5 @@ model = YOLO("yolov8n.yaml")
 # Use the model
 model.train(data="C:/Users/Neyma/OneDrive/Documents/Bristol Uni/2023-24 Modules/MDM3/Phase 3/Augment/Car detection.v1i.yolov8/data.yaml", epochs=3)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
-results = model("C:/Users/Neyma/AppData/Local/Temp/best_electric_luxury_car_bmw_i7.jpg")  # predict on an image
+results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 path = model.export(format="onnx")  # export the model to ONNX format
