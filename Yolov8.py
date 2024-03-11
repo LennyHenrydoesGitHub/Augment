f0eff4afa55336b89873b6400b4be0c09fa3e2e7
Neymat <wq20750@bristol.ac.uk>
2024-03-11 15:13:40 +0000

    code

--- Yolov8.py
+++ Yolov8.py
@@ -11,11 +11,11 @@ from ultralytics import YOLO
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 # Load a model
-#model = YOLO("yolov8n.yaml")
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8n.yaml")
+#model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-model.train(data="coco128.yaml", epochs=3)  # train the model
+model.train(data="C:/Users/Neyma/OneDrive/Documents/Bristol Uni/2023-24 Modules/MDM3/Phase 3/Augment/Car detection.v1i.yolov8/data.yaml", epochs=3)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
-results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+results = model("C:/Users/Neyma/AppData/Local/Temp/best_electric_luxury_car_bmw_i7.jpg")  # predict on an image
 path = model.export(format="onnx")  # export the model to ONNX format
